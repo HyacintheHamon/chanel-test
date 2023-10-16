cb3d3d6842056b1330819248484a8892d85ec02d
Hyacinthe Hamon <hyacinthehamon@Hyacinthes-MacBook-Air.local>
2023-10-16 08:18:57 +0400

    [WIP] - Add animated transition

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -1,13 +1,24 @@
 import { MenuItems } from "@components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { MenuProps, MenuItem } from "@types";
+import { useTransition, animated, useSpringRef } from "@react-spring/web";
 
 export function Menu({ data }: MenuProps) {
   const [currentMenu, setCurrentMenu] = useState<any>(data.items);
   const [tracking, setTracking] = useState<string[]>([]);
 
+  const transRef = useSpringRef();
+  const transitions = useTransition(currentMenu, {
+    ref: transRef,
+    keys: null,
+    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+  });
+
   const handleSelected = (item: any, index: number) => {
+    transRef.start();
     setCurrentMenu(currentMenu[index].nextItems);
 
     if (tracking.length > 0) {
@@ -33,8 +44,12 @@ export function Menu({ data }: MenuProps) {
     setTracking(newTracking);
   };
 
-  return (
-    <div className="flex-col margin-auto mx-32 my-12">
+  useEffect(() => {
+    transRef.start();
+  }, [currentMenu]);
+
+  return transitions((style) => (
+    <div className="flex-col margin-auto mx-32 my-12 overflow-hidden bg-lightGray">
       <div className="bg-lightGray hover:bg-lightBlue p-5 w-full flex flex-row justify-between align-middle cursor-pointer group">
         {tracking.length > 0 ? (
           <button onClick={handleGoBack}>
@@ -47,17 +62,19 @@ export function Menu({ data }: MenuProps) {
           <h2 className="text-dark text-xl font-medium">{data.title}</h2>
         )}
       </div>
-      {currentMenu.map((item: MenuItem, index: number) => {
-        return (
-          <MenuItems
-            items={item}
-            key={index}
-            onSelected={() => {
-              handleSelected(item, index);
-            }}
-          />
-        );
-      })}
+      <animated.div style={style}>
+        {currentMenu.map((item: MenuItem, index: number) => {
+          return (
+            <MenuItems
+              items={item}
+              key={index}
+              onSelected={() => {
+                handleSelected(item, index);
+              }}
+            />
+          );
+        })}
+      </animated.div>
     </div>
-  );
+  ));
 }
