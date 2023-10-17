d0942c032f5a5fd7f97f682591e17a4fdb8b112c
Hyacinthe Hamon <HyacintheHamon@users.noreply.github.com>
2023-10-17 07:34:09 +0200

    Animated transition (#1)
    * [WIP] - Add animated transition
    
    * Improved transition render
    
    * removed transref start duplicate
    
    * Added exitBeforeEnter prop to make sure any items leaving animate out before the new items animate in
    
    * removed ref
    
    * Added tiny rule for responsiveness
    
    * Tweaked duration value
    
    * Extracted Menu into a component
    
    ---------
    
    Co-authored-by: Hyacinthe Hamon <hyacinthehamon@Hyacinthes-MacBook-Air.local>

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,10 +2,20 @@ import { MenuItems } from "@components";
 import { useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
 import { MenuProps, MenuItem } from "@types";
+import { useTransition, animated } from "@react-spring/web";
 
 export function Menu({ data }: MenuProps) {
   const [currentMenu, setCurrentMenu] = useState<any>(data.items);
   const [tracking, setTracking] = useState<string[]>([]);
+
+  const transitions = useTransition(currentMenu, {
+    keys: null,
+    exitBeforeEnter: true,
+    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+    config: { duration: 300 },
+  });
 
   const handleSelected = (item: any, index: number) => {
     setCurrentMenu(currentMenu[index].nextItems);
@@ -34,7 +44,7 @@ export function Menu({ data }: MenuProps) {
   };
 
   return (
-    <div className="flex-col margin-auto mx-32 my-12">
+    <div className="flex-col margin-auto sm:mx-32 sm:my-12 overflow-hidden bg-lightGray">
       <div className="bg-lightGray hover:bg-lightBlue p-5 w-full flex flex-row justify-between align-middle cursor-pointer group">
         {tracking.length > 0 ? (
           <button onClick={handleGoBack}>
@@ -47,15 +57,17 @@ export function Menu({ data }: MenuProps) {
           <h2 className="text-dark text-xl font-medium">{data.title}</h2>
         )}
       </div>
-      {currentMenu.map((item: MenuItem, index: number) => {
+
+      {transitions((style, item: MenuItem, t, i) => {
         return (
-          <MenuItems
-            items={item}
-            key={index}
-            onSelected={() => {
-              handleSelected(item, index);
-            }}
-          />
+          <animated.div style={style} key={i}>
+            <MenuItems
+              items={item}
+              onSelected={() => {
+                handleSelected(item, i);
+              }}
+            />
+          </animated.div>
         );
       })}
     </div>
