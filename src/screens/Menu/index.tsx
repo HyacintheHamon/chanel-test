b7723b10dd79576b54a39057d0d8e0847bc3e722
Hyacinthe Hamon <hyacinthehamon@Hyacinthes-MacBook-Air.local>
2023-10-17 13:25:41 +0800

    Tweaked duration value

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -14,6 +14,7 @@ export function Menu({ data }: MenuProps) {
     from: { opacity: 0, transform: "translate3d(100%,0,0)" },
     enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
     leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+    config: { duration: 300 },
   });
 
   const handleSelected = (item: any, index: number) => {
