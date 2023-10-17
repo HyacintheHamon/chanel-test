a356b16a0b0f706cee199b5fb07a38f7c69ea4de
Hyacinthe Hamon <hyacinthehamon@Hyacinthes-MacBook-Air.local>
2023-10-17 13:33:23 +0800

    Extracted Menu into a component

--- src/main.tsx
+++ src/main.tsx
@@ -1,100 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Menu } from "@screens";
+import App from "@screens/App";
 import "./index.css";
-
-const data = {
-  title: "designplox",
-  items: [
-    {
-      previous: "start",
-      id: "albums",
-      title: "albums",
-      nextItems: [
-        {
-          previous: "albums",
-          id: "album-wings",
-          title: "get your wings",
-          nextItems: [
-            {
-              previous: "album-wings",
-              id: "song-wings-1",
-              title: "lorem ipsum - wings 1",
-            },
-            {
-              previous: "album-wings",
-              id: "song-wings-2",
-              title: "lorem ipsum - wings 2",
-            },
-            {
-              previous: "album-wings",
-              id: "song-wings-3",
-              title: "lorem ipsum - wings 3",
-            },
-          ],
-        },
-        {
-          previous: "albums",
-          id: "album-hotel-california",
-          title: "hotel california",
-          nextItems: [
-            {
-              previous: "album-hotel-california",
-              id: "song-california-1",
-              title: "lorem ipsum - california 1",
-            },
-            {
-              previous: "album-hotel-california",
-              id: "song-california-2",
-              title: "lorem ipsum - california 2",
-            },
-            {
-              previous: "album-hotel-california",
-              id: "song-california-3",
-              title: "lorem ipsum - california 3",
-            },
-          ],
-        },
-        {
-          previous: "albums",
-          id: "album-physical-graffiti",
-          title: "physical graffiti",
-          nextItems: [
-            {
-              previous: "album-physical-graffiti",
-              id: "song-graffiti-1",
-              title: "lorem ipsum - graffiti 1",
-            },
-            {
-              previous: "album-physical-graffiti",
-              id: "song-graffiti-2",
-              title: "lorem ipsum - graffiti 2",
-            },
-            {
-              previous: "album-physical-graffiti",
-              id: "song-graffiti-3",
-              title: "lorem ipsum - graffiti 3",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: "artists",
-      title: "artists",
-      items: [
-        {
-          id: "artist-dali",
-          title: "Dali",
-          nextItems: [],
-        },
-      ],
-    },
-  ],
-};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Menu data={data} />
+    <App />
   </React.StrictMode>
 );
